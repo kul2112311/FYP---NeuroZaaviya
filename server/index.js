7f497989fee1b8f9bf11bcc3d079f9009fbd29c8
Kulsoom Asim <107303486+kul2112311@users.noreply.github.com>
2026-05-14 21:19:38 +0500

    Update CORS configuration to allow more methods

--- server/index.js
+++ server/index.js
@@ -28,10 +28,11 @@ const contextService = require('./services/ai/contextService');
 const app = express();
 
 app.use(cors({
-  origin: 'https://fyp-neuro-zaaviya.vercel.app'
+  origin: 'https://fyp-neuro-zaaviya.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
-
 
 const upload = multer({ storage: multer.memoryStorage() });
 const kulsoom_index = require('./kulsoom_index');
